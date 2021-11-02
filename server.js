ab682cfbeb13fb0f5569b7987513a80e4cd818f4
JeniLopez <jelopez@escuelasproa.edu.ar>
2021-11-01 22:52:35 -0300

    codigo actividad(no se xq no anda)

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
-const express = require ('express');
+const express = require("express");
+const bodyParser = require("body-Parser");
 const app = express();
-const port = 3000;
+const port = 2250;
+app.use(express());
+app.use(bodyParser());
 
+app.listen(port, () => {
+    console.log("hi");
+});
+
+app.get("/hi", (req, res) => {
+    res.send(`Servidor corriendo en el puerto local ${port}`);
+});
+
+app.post("/precioFrutas", (req, res) => {
+    let precio = 0;
+    for (x of req.body) {
+        if (i.tipo == "fruta") {
+            precio += i.precio;
+        }
+    }
+    res.send(`El valor total de las frutas es ${precio}`);
+});
