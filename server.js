920a19c19974b2b3d63a2f18c858d3180b54064b
JeniLopez <jelopez@escuelasproa.edu.ar>
2021-11-02 23:05:43 -0300

    actividad matrices (sin terminar)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-Parser");
+const { json } = require("express");
 const app = express();
 const port = 2250;
 app.use(express());
@@ -15,10 +16,18 @@ app.get("/hi", (req, res) => {
 
 app.post("/precioFrutas", (req, res) => {
     let precio = 0;
-    for (x of req.body) {
-        if (i.tipo == "fruta") {
-            precio += i.precio;
+    let json = req.body;
+
+    for (x in json) {
+        if (json.x.tipo == "fruta") {
+            precio += json.x.precio;
         }
     }
     res.send(`El valor total de las frutas es ${precio}`);
+});
+
+app.post("/determinante", (req, res) => {
+    let determinante =
+        req.body[0][0] * req.body[1][1] - req.body[1][0] * req.body[0][1];
+    res.send(`El determinante es ${determinante}`);
 });
